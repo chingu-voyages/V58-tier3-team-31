3c14a6b7ff2faedb639b0624d6654b1544836f95
Peter Tasca <tascapeter@gmail.com>
2025-11-10 17:00:51 -0500

    Feature/reactnative (#61)
    * fix(workflow): remove supabase version
    
    remove supabase version
    
    * chore(safestep): install react native in mobile and extend ts config
    
    install react native in mobile and extend ts config
    
    * feat(mobile): react native dev env and metro config set up
    
    set up react native app in mobiile folder, included metro config setup, and extended ts config in
    mobile
    
    * fix(safestep): reinstalled pnpm lockfile and updated PR request template
    
    reinstalled pnpm lockfile and updated PR request template
    
    * style(mobile): refactored imports for biome
    
    refactor imports for biome
    
    * chore(safestep): updated package.json
    
    updated typescript and biome versions in package.json. included linting scripts
    
    * chore(safestep): reinstalled biome and added lint scripts
    
    reinstalled biome and added lint scripts
    
    * chore(safestep): resintalled biome and added scripts at root
    
    resintalled biome and added scripts at root

---
+++ mobile/components/haptic-tab.tsx
@@ -0,0 +1,19 @@
+import type { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+import { PlatformPressable } from "@react-navigation/elements";
+import type { GestureResponderEvent } from "react-native";
+import * as Haptics from "expo-haptics";
+
+export function HapticTab(props: BottomTabBarButtonProps) {
+	return (
+		<PlatformPressable
+			{...props}
+			onPressIn={(ev: GestureResponderEvent) => {
+				if (process.env.EXPO_OS === "ios") {
+					// Add a soft haptic feedback when pressing down on the tabs.
+					Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+				}
+				props.onPressIn?.(ev);
+			}}
+		/>
+	);
+}

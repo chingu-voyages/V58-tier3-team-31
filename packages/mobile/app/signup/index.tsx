eea267c3d8b62a75443ba4459db3da95affbe40e
petertasca <tascapeter@gmail.com>
2025-11-11 16:54:29 -0500

    feat(safestep): setup authentication and supabase client connection
    setup authentication and supabase client connection

--- packages/mobile/app/signup/index.tsx
+++ packages/mobile/app/signup/index.tsx
@@ -1,27 +1,26 @@
-// import { TextInput, View, Text } from "react-native";
-// import { useState } from "react";
+import "react-native-url-polyfill";
+import { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { View, Text } from "react-native";
+import type { Session } from "@supabase/supabase-js";
 import Auth from "../components/Auth";
 
 export default function Signup() {
-	// const [email, setEmail] = useState("");
-	// const [password, setPassword] = useState("");
+	const [session, setSession] = useState<Session | null>(null);
+
+	useEffect(() => {
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			setSession(session);
+		});
+		supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session);
+		});
+	}, []);
 
 	return (
-		<Auth />
-		// <View>
-		// 	<Text>Email</Text>
-		// 	<TextInput
-		// 		placeholder="Enter your email"
-		// 		onChangeText={(newEmail) => setEmail(newEmail)}
-		// 		defaultValue={email}
-		// 	></TextInput>
-		// 	<Text>Password</Text>
-		// 	<TextInput
-		// 		placeholder="Enter a password"
-		// 		onChangeText={(newPassword) => setPassword(newPassword)}
-		// 		defaultValue={password}
-		// 		secureTextEntry={true}
-		// 	></TextInput>
-		// </View>
+		<View>
+			<Auth />
+			{session?.user?.id && <Text>{session.user.id}</Text>}
+		</View>
 	);
 }

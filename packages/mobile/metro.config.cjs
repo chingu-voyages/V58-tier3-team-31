a5d970e5265206441d10fdc0d9f2c891bb0809df
petertasca <tascapeter@gmail.com>
2025-11-12 14:11:24 -0500

    fix(mobile): no routes found bug -- set metro.config
    no routes found bug -- set metro.config

---
+++ packages/mobile/metro.config.cjs
@@ -0,0 +1,26 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../../');
+
+require('@expo/env').load(workspaceRoot, { force: true });
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [
+  projectRoot,
+  workspaceRoot,
+];
+
+// You might also need to configure `resolver.nodeModulesPaths` for monorepos
+   config.resolver.nodeModulesPaths = [
+     path.resolve(projectRoot, 'node_modules'),
+     path.resolve(workspaceRoot, 'node_modules'),
+   ];
+
+// config.resolver.alias = {
+// 	"@": __dirname,
+// };
+config.resolver.assetExts.push('db');
+module.exports = config;

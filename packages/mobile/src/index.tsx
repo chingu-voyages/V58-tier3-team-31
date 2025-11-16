47423e6d72b21e02b44177bf85999f6c658cee30
petertasca <tascapeter@gmail.com>
2025-11-16 12:03:12 -0500

    feat(mobile): react native reusables installed
    react native reusables installed

--- packages/mobile/src/index.tsx
+++ packages/mobile/src/index.tsx
@@ -1,11 +1,5 @@
-// import UserConsentLocationServices from "./components/UserConsentLocationServices";
-import { Text, View } from "react-native";
+import UserConsentLocationServices from "./components/UserConsentLocationServices";
 
 export default function Index() {
-	return (
-		// <UserConsentLocationServices />
-		<View className="flex-1 items-center bg-white border-4 border-black">
-			<Text className="font-bold text-blue-500">Welcome to Nativewind!</Text>
-		</View>
-	);
+	return <UserConsentLocationServices />;
 }
--- packages/mobile/tailwind.config.js
+++ packages/mobile/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "pine-green": "#01796F"
+
+        }
+    },
   },
   plugins: [],
 }
+
+// Forest Green	#228B22	Classic, rich, natural dark green.
+// Hunter Green	#355E3B	A very deep, sophisticated, almost black-green.
+// Pine Green	#01796F	A dark cyan-green, slightly cooler than forest green.
+// Dark Olive Green	#556B2F	An earthy, muted dark green with a yellow undertone.
+// Deep Emerald	#046307	A very dark, vibrant green often used for high contrast.
+// A Custom Safe Green	#1A4314	A darker shade that works well for primary buttons against a light background.

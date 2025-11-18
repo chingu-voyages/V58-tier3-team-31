c98d72f4aaa15447ff5c54d89a1ca4bb59a329ba
fmtabbara <fmtabbara@hotmail.co.uk>
2025-11-18 12:11:22 +0000

    chore: rebase

--- packages/mobile/app/_layout.tsx
+++ packages/mobile/app/_layout.tsx
@@ -1,6 +1,25 @@
-import "react-native-url-polyfill";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
+
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
 
 export default function RootLayout() {
-	return <Stack />;
+  return (
+    <GluestackUIProvider mode="dark">
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
+      </Stack>
+      <StatusBar style="auto" />
+    </GluestackUIProvider>
+  );
 }

61dbb26df067e5bbd57b91a884b5fb6bb1f5ffe4
Peter Tasca <tascapeter@gmail.com>
2025-11-18 16:30:10 -0500

    Feature/auth (#64)
    * refactor(mobile): reset project and started signup page
    
    reset project and started signup page
    
    * feat(mobile): set up dotenv
    
    set up babel.config.ts for dotenv plugin and env variables for supabase client
    
    * chore(safestep): set up env variables, configured supabase client, and auth
    
    set up env variables, configured supabase client, and created auth component
    
    * feat(safestep): setup authentication and supabase client connection
    
    setup authentication and supabase client connection
    
    * refactor(mobile): moved auth functions to useAuth and useSession hooks
    
    moved auth functions to useAuth and useSession hooks. added loading and error states for mobile
    client
    
    * chore(mobile): uninstalled es lint. installed biome
    
    uninstalled eslint. installed biome as a dev dependency in mobile
    
    * chore(mobile): reconfigured tsconfig.base.json and tsconfig in mobile
    
    reconfigured tsconfig.base.json and tsconfig in mobile
    
    * refactor(mobile): changed app to src and updated config to point at root
    
    changed app to src and updated config to point at root
    
    * refactor(mobile): add try/catch blocks to useSession and useAuth
    
    add try/catch blocks to useSession and useAuth
    
    * chore(safestep): added scripts to root package.json
    
    added scripts to root package.json
    
    * chore(safestep): reconfigured package.json and reinstalled dependencies
    
    reconfigured package.json and reinstalled dependencies
    
    * refactor(mobile): removed logs and comments. refactored auth hook into utility functions
    
    removed logs and comments. refactored auth hook into utility functions
    
    * refactor(mobile): refactored useSession hook
    
    refactored useSession hook
    
    * refactor(mobile): changed src to app. updated config
    
    changed src to app. updated config
    
    * refactor(tsconfig.base.json): removed jsx from tsconfig.base.json
    
    removed jsx from tsconfig.base.json

---
+++ packages/mobile/app/hooks/useSession.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import { Alert } from "react-native";
+import type { Session, User } from "@supabase/supabase-js";
+
+const fetchSession = async () => {
+	try {
+		const {
+			data: { session },
+			error,
+		} = await supabase.auth.getSession();
+
+		if (error) {
+			Alert.alert(error.message);
+		} else {
+			return session;
+		}
+	} catch (err) {
+		Alert.alert("Error fetching session state.");
+		console.error("Session fetch failed:", err);
+	}
+};
+
+const useSession = () => {
+	const [session, setSession] = useState<Session | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		const createSession = async () => {
+			setIsLoading(true);
+			try {
+				const session = await fetchSession();
+				setSession(session || null);
+			} catch (err) {
+				console.error("Failed to fetch session:", err);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		createSession();
+
+		const { data: listener } = supabase.auth.onAuthStateChange(
+			(_event, session) => {
+				setSession(session);
+			},
+		);
+
+		return () => {
+			listener?.subscription.unsubscribe();
+		};
+	}, []);
+
+	return {
+		session,
+		user: session?.user as User | undefined,
+		isLoading,
+	};
+};
+
+export default useSession;

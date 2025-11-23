b55ebf9f8a0b1c9d87a0b177a1d23ea7623a3770
petertasca <tascapeter@gmail.com>
2025-11-23 12:39:46 -0500

    refactor(app/(private)): conditional routing refactor with initialRouteName
    conditional routing refactor with initialRouteName

--- packages/mobile/src/app/(private)/_layout.tsx
+++ packages/mobile/src/app/(private)/_layout.tsx
@@ -2,9 +2,6 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import useUserRole from "@/hooks/useUserRole";
-import { useRouter } from "expo-router";
-import { useEffect } from "react";
-
 export const unstable_settings = {
 	// Anchor ensures that tabs in this layout only control private screens
 
@@ -12,23 +9,20 @@ export const unstable_settings = {
 };
 
 const PrivateLayout = () => {
-	const router = useRouter();
 	const { userRole, isLoading } = useUserRole();
-
-	useEffect(() => {
-		if (!isLoading && userRole) {
-			if (userRole.role === "recoverer") router.replace("/(private)/recoverer");
-			if (userRole.role === "sponsor") router.replace("/(private)/sponsor");
-		}
-	}, [isLoading, userRole, router]);
 
 	if (isLoading) return null;
 
 	return (
 		<GluestackUIProvider mode="dark">
-			<Stack>
-				<Stack.Screen name="sponsor" options={{ headerShown: false }} />
-				<Stack.Screen name="recoverer" options={{ headerShown: false }} />
+			<Stack
+				screenOptions={{ headerShown: false }}
+				initialRouteName={
+					userRole?.role === "recoverer" ? "recoverer" : "sponsor"
+				}
+			>
+				<Stack.Screen name="sponsor" />
+				<Stack.Screen name="recoverer" />
 			</Stack>
 			<StatusBar style="auto" />
 		</GluestackUIProvider>

56b45861b6f51228594043ad98b8a5b67a0f72ea
Fouad <fmtabbara@hotmail.co.uk>
2025-11-26 15:37:37 +0000

    refactor: simplify location tracking start stop and permission (#82)

--- packages/mobile/src/types/geolocation.ts
+++
@@ -1,4 +0,0 @@
-import type Location from "expo-location";
-
-export type LocationSubscription = Location.LocationSubscription | null;
-export type LocationCoords = { latitude: number; longitude: number } | null;

74058c2b8811bcac46bbbbb34d6d731cefd07468
petertasca <tascapeter@gmail.com>
2025-11-27 10:13:18 -0500

    feat(suapbase): created edge function for resend email
    created edge function for resend email

--- packages/api/supabase/functions/invite-sponsor/index.ts
+++ packages/api/supabase/functions/invite-sponsor/index.ts
@@ -1,24 +1,44 @@
 
-import React from "react";
+
 import { Resend } from "resend";
-import { renderAsync } from "@react-email/components";
-import { MagicLinkEmail } from "./_templates/magic-link.tsx";
+import {create, getNumericDate } from "https://deno.land/x/djwt@v2.11/mod.ts";
 
 const resend = new Resend(Deno.env.get("RESEND_API_KEY") as string);
 
 Deno.serve(async (req) => {
-  const { email, token } = await req.json();
 
-  const html = await renderAsync(
-    React.createElement(MagicLinkEmail, { token })
-  );
+  if (req.method !== "POST") return new Response(JSON.stringify({ error: "Method not allowed"}), { status: 405});
+
+  try {
+
+    const { email, phone, message } = await req.json();
+
+    if (!email) return new Response(JSON.stringify({ error: "Missing email"}), { status: 400});
+
+    const jwtSecret = Deno.env.get("SUPABASE_JWT_SECRET");
+    const token = await create(
+      {alg: "HS256", typ: "JWT"},
+      { email, exp: getNumericDate(60 * 60)},
+      jwtSecret
+    );
+
+    const registrationUrl = "http://localhost:8081/signup"
+
+    const html = `<p>${message}</p> <p> <a href="${registrationUrl}">Click here to register</a></p>`
 
   await resend.emails.send({
     from: "SafeStep <noreply@safestep.com>",
     to: email,
-    subject: "You're Invited!",
+    subject: "You've been invited to be a sponsor!",
     html
   });
 
   return new Response(JSON.stringify({ status: "ok" }), { status: 200 });
+
+  } catch (err) {
+    console.error("There was an error sending your email:", err);
+    return new Response(JSON.stringify({error: "Failed to send email"}), { status: 500})
+  }
+
+  
 });

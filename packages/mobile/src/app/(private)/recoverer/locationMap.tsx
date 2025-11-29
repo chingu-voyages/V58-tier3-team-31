a357bc6d2efd2d384d3d2e896256d28e94cb8823
petertasca <tascapeter@gmail.com>
2025-11-29 10:32:09 -0500

    refactor(app): react native maps installed but imports not working
    react native maps installed but imports not working

--- packages/mobile/src/app/(private)/recoverer/locationMap.tsx
+++ packages/mobile/src/app/(private)/recoverer/locationMap.tsx
@@ -1,3 +1,7 @@
+import useLocationTracker from "@/hooks/useLocationTracker";
+import { useRef, useEffect, useState } from "react";
+import { Dimensions } from "react-native";
+
 const LocationMap = () => {};
 
 export default LocationMap;
--- packages/mobile/src/app/(private)/sponsor/index.tsx
+++ packages/mobile/src/app/(private)/sponsor/index.tsx
@@ -7,8 +7,6 @@ const SponsorDashboard = () => {
 	if (isLoading) return <Text>Loading...</Text>;
 	if (!sponsor) return <Text>Sponsor not found</Text>;
 
-	console.log("sponsor:", sponsor);
-
 	return (
 		<View>
 			<Text>Welcome, {sponsor?.firstName}</Text>

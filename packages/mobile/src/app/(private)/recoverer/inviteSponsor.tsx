b3068a7db3eb3b7fcf7b3dd4d8fb32a064ef00cf
Peter Tasca <tascapeter@gmail.com>
2025-12-14 10:12:54 -0500

    Feature/sponsor invitation email (#85)
    * feat(mobile/app): stack navigation and layout created for auth
    
    stack navigation and layout created for auth
    
    * feat(mobile/app): sign in and sign up bottom tabs added
    
    sign in and sign up bottom tabs added
    
    * feat(mobile/app): user role sign in implemented
    
    user role sign in implemented
    
    * feat(app/hooks): user role hook
    
    user role hook
    
    * feat(mobile/app): conditional routing for recoverer and sponsor
    
    conditional routing for recoverer and sponsor
    
    * refactor(app/(private)): conditional routing refactor with initialRouteName
    
    conditional routing refactor with initialRouteName
    
    * feat(mobile/app/(private)): logout functionality and redirect
    
    logout functionality and redirect
    
    * style(app): removed console logs
    
    removed console logs
    
    * fix(pnpm-lock file): fix dependencies
    
    * refactor: simplify location tracking start stop and permission (#82)
    
    * feat(app): rebased main
    
    rebased main
    
    * feat(safestep): rebase complete
    
    rebase complete
    
    * fix(app): remove link
    
    remove link
    
    * feat(component): create a inviteSponsor folder
    
    * feat(invitesponsor): create the UI for the inviteSponsor page
    
    * feat(app): inviteSponsor page
    
    inviteSponsor page
    
    * feat(supabase): deno and imports configured for resend edge function
    
    deno and imports configured for resend edge function
    
    * feat(suapbase): created edge function for resend email
    
    created edge function for resend email
    
    * feat(app): supabase edge function handler on client created
    
    supabase edge function handler on client created
    
    * feat(supabase): supabase edge function with resend working
    
    supabase edge function with resend working
    
    * refactor(app): fixed styling and add inivte sponsor button
    
    fixed styling and add inivte sponsor button
    
    * feat(app): wired up handle invite sponsor and removed imports
    
    wired up handle invite sponsor and removed imports
    
    * feat(app): created native and web map components along with declaration file
    
    created native and web map components along with declaration file
    
    * feat(tsconfig.json): removed platform specific files from tsconfig.json
    
    removed platform specific files from tsconfig.json
    
    ---------
    
    Co-authored-by: TEMESGEN-G25 <temesgen.g5684@gmail.com>
    Co-authored-by: Fouad <fmtabbara@hotmail.co.uk>

--- packages/mobile/src/app/(private)/recoverer/inviteSponsor.tsx
+++ packages/mobile/src/app/(private)/recoverer/inviteSponsor.tsx
@@ -1,11 +1,121 @@
-import { View, Text } from "react-native";
+import { VStack } from "@/components/ui/vstack";
+import { Input, InputField } from "@/components/ui/input";
+import { FormControl } from "@/components/ui/form-control";
+import { Textarea, TextareaInput } from "@/components/ui/textarea";
+import { useState } from "react";
+import { Button, ButtonText, ButtonSpinner } from "@/components/ui/button";
+import { Alert } from "react-native";
+import useSession from "@/hooks/useSession";
+import { supabase } from "@/lib/supabase";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "@/components/ui/text";
+import { Heading } from "@/components/ui/heading";
+import { HStack } from "@/components/ui/hstack";
 
-const InviteSponsor = () => {
+const inviteSponsor = () => {
+  const { session } = useSession();
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInviteSponsor = async () => {
+    setIsLoading(true);
+
+    try {
+      const { data, error } = await supabase.functions.invoke(
+        "invite-sponsor",
+        {
+          body: JSON.stringify({
+            message: message,
+            phone: phone,
+            email: email,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.access_token}`,
+          },
+        },
+      );
+
+      if (error) {
+        console.error("Error sending email:", error);
+        Alert.alert("Failed to send invite. Check console for details");
+        return;
+      }
+
+      console.log("edge function data:", data);
+
+      Alert.alert("Sponsor invidation sent successfully!");
+      setEmail("");
+      setPhone("");
+      setMessage("");
+    } catch (err) {
+      console.error("Network or server error:", err);
+      Alert.alert("Something went wrong. Check the console for details");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>Invite Sponsor Page</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1 flex flex-col justify-center">
+      <FormControl className="p-8 flex flex-col gap-3">
+        <VStack space="lg">
+          <Heading className="text-center text-3xl font-bold text-primary-500">
+            Invite a Sponsor
+          </Heading>
+          <Text className="font-light">
+            Your sponsor will receive a message with instructions on how to
+            download the app and connect with you.
+          </Text>
+          <VStack space="md">
+            <Input variant="outline">
+              <InputField placeholder="Sponsor's Email" />
+            </Input>
+            <Input variant="outline" className="">
+              <InputField placeholder="Sponsor's Phone Number" />
+            </Input>
+          </VStack>
+          <VStack>
+            <Heading className="text-left text-2xl font-bold text-primary-500">
+              Add a Personal Message
+            </Heading>
+            <Textarea
+              isDisabled={isLoading}
+              className="border rounded-8 bg-white"
+            >
+              {" "}
+              <TextareaInput placeholder="Hi [Sponsor's Name], I'd like to invite you to be my sponsor on Safestep" />{" "}
+            </Textarea>
+          </VStack>
+          <VStack className="mt-[12px]">
+            <Heading className="text-left text-2xl font-bold text-primary-500">
+              Tags
+            </Heading>
+            <Text className="font-light">
+              Assign tags to help define your relationship with your sponsor.
+            </Text>
+          </VStack>
+          <HStack space="lg">
+            <Text className="border-0 rounded-[100px] bg-gray-200 p-2 flex flex-col justify-center text-sm font-light text-center border-black">
+              Therapist
+            </Text>
+            <Text className="border border-dashed rounded-[100px] bg-gray-200  p-2 flex flex-col justify-center text-sm font-light">
+              Family
+            </Text>
+            <Text className="border border-dashed rounded-[100px] p-2 text-center text-sm font-light">
+              Add Tag +
+            </Text>
+          </HStack>
+        </VStack>
+        <Button onPress={handleInviteSponsor} className="rounded-[100px]">
+          <ButtonText>Send Invitation</ButtonText>
+          {isLoading && <ButtonSpinner />}
+        </Button>
+      </FormControl>
+    </SafeAreaView>
   );
 };
 
-export default InviteSponsor;
+export default inviteSponsor;
---
+++ packages/mobile/src/components/custom/LocationMap/index.web.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const LocationMap = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>Map is not available on web.</Text>
+    </View>
+  );
+};
+
+export default LocationMap;
